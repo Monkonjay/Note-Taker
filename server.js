8b9f398d38b04c6c986ec37a1a772e5264bbd4eb
R Greene <greenbert81@gmail.com>
2022-09-05 19:33:02 -0700

    added post routes and fixed css link

--- server.js
+++ server.js
@@ -1,62 +1,68 @@
 const express = require('express');
+const { fstat } = require('fs');
 const path = require('path');
-// const db = require('./db');
-
-
 const app = express();
+const notes = require('./db/db.json');
+
+// middleware for public folder
+app.use(express.static('public'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+
+
 const PORT = 3001;
 
-
-// GET Route for index.html
+// get route returning index.html
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
-// get route for notes.html
+
+// get route returning notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+// get routes for returning all notes.
+app.get('/api/notes', (req, res) => {
+res.json(notes);
 });
 
 
 
-// // Invoke app.use() and serve static files from the '/public' folder
-// app.use(express.static('public'));
-// // Middleware for parsing JSON and urlencoded form data
-// app.use(express.urlencoded({extended : true}));
-// app.use(express.json());
+// POST route for /api/routes to create a new note.
+app.post('/api/notes', (req,res) => {
+req.body.id = Math.floor(Math.random() * 1000000);
+notes.push(req.body);
+fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+res.json(notes);
+  
+ 
+});
+
+// GET Route to retrive note with specific id.
+app.get('/api/notes/:id' , (req, res) => {
+
+});
 
 
-// // GET Routes for /api/notes to return all notes.
-// app.get('/api/notes', (req, res) => {
-//   return res.json(db.getAllNotes());
-// });
+// Delete note with the given id
+app.delete('/api/notes/:id' , (req,res) => {
 
+  for(i = 0; i < notes.length; i ++) { 
+    if(notes[i].id === req.body.id) {
+      notes.splice(notes.indexOf(req.body.id,1));
+    }
 
-
-// // POST route for /api/routes to create a new note.
-// app.post('/api/notes', (req,res) => {
-//   // extract title and text of the note from request body.
-//   let {title, text} = req.body;
-//   // Create a new note in the DB.
-//   db.createNote(title, text).then((result) => res.json(result)).catch((err) => res.json(err));
-// });
-
-// // GET Route to retrive note with specific id.
-// app.get('/api/notes/:id' , (req, res) => {
-//   // fetch specific note from db and send it back.
-//   res.json(db.getNoteWithId(req.params.id));
-// });
-
-
-// // Delete note with the given id
-// app.delete('/api/notes/:id' , (req,res) => {
-//   // Delete the note from DB and respond back with deleted note if succeeded, err if failed.
-//   db.deleteNoteWithId(req.params.id).then((result) => res.json(result)).catch((err) => res.json(err));
-// });
+  }
+  
+});
 
 // server listening 
 app.listen(PORT, () => {
-  // Initialize notes DB on 
-//   db.initiateNotesDB();
   console.log(`Server listening for request on port ${PORT}`)
 });
